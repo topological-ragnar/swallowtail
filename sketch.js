8dbf29079b219b88e9c8fcf74f612cbad491df73
Ryan Thorngren <ryan.thorngren@gmail.com>
2020-10-02 21:33:34 -0700

    dumb trippy ball thing

--- sketch.js
+++ sketch.js
@@ -1,5 +1,6 @@
 const $ = require('jquery')
 const THREE = require('three')
+OrbitControls = require('three-orbit-controls')
 const dat = require('dat.gui')
 const loop = require('raf-loop')
 const WindowResize = require('three-window-resize')
@@ -9,8 +10,12 @@ class Sketch {
 
     constructor() {
         this.gui = new dat.GUI()
-        // this.params = gui.addFolder('parameters');
-        // gui.show()
+        this.params = this.gui.addFolder('parameters');
+        this.gui.show()
+        this.params.xspeed = 1
+        this.params.yspeed = 1
+        this.gui.add(this.params, 'xspeed', 0, 10);
+        this.gui.add(this.params, 'yspeed', 0, 10);
         
         this.scene = new THREE.Scene()
         this.camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000)
@@ -19,13 +24,25 @@ class Sketch {
         document.body.appendChild(this.renderer.domElement);
         var windowResize = new WindowResize(this.renderer, this.camera)
 
-        var geometry = new THREE.BoxGeometry();
+        
+
+
+        var geometry = new THREE.BoxGeometry(2,1,1);
         // var material = new THREE.MeshBasicMaterial({ color: 0x00ff00 });
         var material = new THREE.MeshNormalMaterial()
-        this.cube = new THREE.Mesh(geometry, material);
-        this.scene.add(this.cube);
+        // this.cube = new THREE.Mesh(geometry, material);
+        // this.scene.add(this.cube);
         this.camera.position.z = 5
 
+        this.numCubes = 100
+        this.cubes = []
+        for(var i = 0; i < this.numCubes; i++){
+            this.cubes.push(new THREE.Mesh(geometry, material))
+            this.scene.add(this.cubes[i])
+        }
+
+
+        // this.controls = new OrbitControls(this.camera, this.renderer.domElement);
 
     }
 
@@ -45,8 +62,13 @@ class Sketch {
     render() {
         // requestAnimationFrame(this.animate);
         this.renderer.render(this.scene, this.camera);
-        this.cube.rotation.x += 0.01;
-        this.cube.rotation.y += 0.01;
+        for(var i = 0; i < this.numCubes; i++){
+            this.cubes[i].rotation.x += 0.0001*this.params.xspeed*i;
+            this.cubes[i].rotation.y += 0.0001*this.params.yspeed*i;
+        }
+        // this.controls.update();
+
+        
     }
     
 
